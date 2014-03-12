1c645e6e6c2d0e207327fc71d1d6f53460b6b728
Robert Krug <robert.krug@oru.se>
2014-03-12 17:43:21 +0100

    include third-party memoize

--- experiments/shift/convert2matlab.m
+++ experiments/shift/convert2matlab.m
@@ -5,6 +5,7 @@ name{2}='Sprayflask_5k';
 name{3}='ShowerGel_5k';
 name{4}='RedCup_5k';
 name{5}='Tortoise_5k';
+name{6}='CokePlasticSmall_5k';
 
 for i=1:numel(name)
     CT=load(strcat(name{i},'_CT.txt'));
